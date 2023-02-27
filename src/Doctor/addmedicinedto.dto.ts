bcbfe7dc25a4dc9855848cb9a6db8a14a12d3168
Arif Ishteak <ishteak01@gmail.com>
2023-02-27 20:25:53 +0600

    arif

--- src/Doctor/addmedicinedto.dto.ts
+++ src/Doctor/addmedicinedto.dto.ts
@@ -1,6 +1,9 @@
 import { IsDate, IsDateString, IsNumber, IsString } from "class-validator"
 export class addmedicinedto{
-    @IsString({message: 'Title is too long. Maximal length is $constraint1 characters, but actual is $value',}) name: string
-     @IsNumber()id: number}
+    @IsString({message: 'Title is too long. Maximal length is $constraint1 characters, but actual is $value',}) 
+    name: string
+    
+     @IsNumber()
+     id: number}
      //@IsNumber()
      // expiredate: Date}
